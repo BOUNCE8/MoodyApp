85467ba77f2200522a6bdaf19b2cd7591cc13683
Anne Berg Jolis <annejolis@gmail.com>
2021-05-12 18:04:49 +0100

    feat: center past mood records on dashboard

--- Screens/Dashboard/DashBoard.style.tsx
+++ Screens/Dashboard/DashBoard.style.tsx
@@ -1,22 +1,27 @@
 import { StyleSheet, Platform, Dimensions } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 const width = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // backgroundColor: '#a178b1',
+    backgroundColor: '#a178b1',
     paddingTop: Platform.OS === 'android' ? 30 : 0,
+    marginLeft: Platform.OS === 'android' ? 30 : 0,
     alignItems: 'center',
   },
   entriesContainer: {
-    alignItems: 'center',
     display: 'flex',
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
     overflow: 'scroll',
     width: width,
     flex: 1,
     borderColor: '#fefefe',
     borderWidth: 2,
+    backgroundColor: 'white',
   },
 });
 
